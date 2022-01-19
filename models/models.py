fad9addf4dda400ad2da05c2715337baf1bc1cd7
ArobyR <ar.rosario001@gmail.com>
2022-01-19 14:44:04 -0400

    Add some methods
    Add _get_products_formated, inherited_action and fix some orthography errors

--- models/models.py
+++ models/models.py
@@ -14,6 +14,29 @@ class Invoice(models.Model):
 
     total = fields.Float(compute="_calc_total", string="Total")
 
+    # First get the information of product_ids
+    # Call the function and return the list formated with the data
+    def _get_products_formated(self):
+        product_lines = []
+        for line in self.product_ids:
+            product_lines.append((0, 0, 
+                    {
+                        "product_id": line.product_id.id,
+                        "account_id": 1,
+                        "name": line.nombre_product,
+                        "quantity": line.cantidad,
+                        "price_unit": line.precio,
+                        "price_subtotal": line.subtotal
+                    }))
+        return product_lines
+
+    def inherited_action(self):
+        self.env["account.move"].create({
+                "partner_id": self.partner_id.id,
+                "invoice_date": self.date_invoice,
+                "line_ids": self._get_products_formated(),
+            })
+
     @api.depends("product_ids.subtotal")
     def _calc_total(self):
         for record in self:
@@ -24,7 +47,7 @@ class Product(models.Model):
     _name = "product.line"
 
     invoice_id = fields.Many2one("invoice.invoice", string="Invoice")
-    produt_id = fields.Many2one("product.product")
+    product_id = fields.Many2one("product.product")
     nombre_product = fields.Char()
     precio = fields.Float(required=True)
     cantidad = fields.Integer(required=True, default=1)
@@ -35,8 +58,8 @@ class Product(models.Model):
         for record in self:
             record.subtotal = record.precio * record.cantidad
 
-    @api.onchange("produt_id")
+    @api.onchange("product_id")
     def _onchange_product_id(self):
-        self.nombre_product = self.produt_id.name
-        self.precio = self.produt_id.list_price
+        self.nombre_product = self.product_id.name
+        self.precio = self.product_id.list_price
 
