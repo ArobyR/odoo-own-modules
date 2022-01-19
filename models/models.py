1f5620093305680d8125bf6bf5472d5a37fee9dd
ArobyR <ar.rosario001@gmail.com>
2022-01-19 16:37:26 -0400

    add a little doc. and change a method name

--- models/models.py
+++ models/models.py
@@ -14,9 +14,16 @@ class Invoice(models.Model):
 
     total = fields.Float(compute="_calc_total", string="Total")
 
-    # First get the information of product_ids
-    # Call the function and return the list formated with the data
-    def _get_products_formated(self):
+    link = ""
+    invoice_generated = fields.Many2one("account.move")
+
+    def _get_products_formatted(self):
+        """ Return a list products formatted
+            Create a void list (product_lines)
+            And then get the information of product_ids
+            later add the values to the product_lines with
+            format for return it.
+        """
         product_lines = []
         for line in self.product_ids:
             product_lines.append((0, 0, 
@@ -31,15 +38,24 @@ class Invoice(models.Model):
         return product_lines
 
     def inherited_action(self):
-        self.env["account.move"].create({
+        """ An action using account.move model
+            the purpose is interact with other module
+            for example in this case we're creating 
+            a invoice in other module with the method create()
+            and finally relates what contains link with invoice 
+            generated in order to do some things
+        """
+        link = self.env["account.move"].create({
                 "partner_id": self.partner_id.id,
                 "invoice_date": self.date_invoice,
-                "invoice_line_ids": self._get_products_formated(),
+                "invoice_line_ids": self._get_products_formatted(),
                 "move_type": "out_invoice",
             })
+        self.invoice_generated = link.id
 
     @api.depends("product_ids.subtotal")
     def _calc_total(self):
+        """ Calculate the total """ 
         for record in self:
             self.total = sum(line.subtotal for line in record.product_ids)
     
@@ -56,11 +72,13 @@ class Product(models.Model):
 
     @api.depends("precio", "cantidad")
     def _calc_subtotal(self):
+        """ Calculate the subtotal """ 
         for record in self:
             record.subtotal = record.precio * record.cantidad
 
     @api.onchange("product_id")
     def _onchange_product_id(self):
+        """ To update the fields """
         self.nombre_product = self.product_id.name
         self.precio = self.product_id.list_price
 
