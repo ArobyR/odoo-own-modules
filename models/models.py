9d7b0054190882404a201967855a6bf4e596b17a
ArobyR <ar.rosario001@gmail.com>
2022-01-28 16:56:25 -0400

    add product_sale module

---
+++ models/models.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductSale(models.Model):
+    _name = 'product.sale'
+    _description = 'Product Sale'
+
+    generated_sale_id = ''
+
+    partner_ids = fields.Many2many('res.partner', string="Partner")
+    product_ids = fields.One2many('product.lines', 'sale_id')
+    total = fields.Float(compute="_calc_total", string="Total")
+
+    def _get_products_formatted(self):
+        product_lines = []
+        for line in self.product_ids:
+            product_lines.append((0, 0, 
+                {
+                    "product_id": line.product_id.id,
+                    "name": line.product_name,
+                    "product_uom_qty": line.quantity,
+                    "price_unit": line.price,
+                    "price_subtotal": line.subtotal
+                }
+            ))
+        return product_lines
+
+    # Create a sale...
+    def create_sale(self):
+        for line in self.partner_ids:
+            self.env['sale.order'].create({
+                "partner_id": line.id,
+                "order_line": self._get_products_formatted(),
+                "date_order": fields.Date.today(),
+                }
+            )
+
+    # Send an email
+    def send_email(self):
+        pass
+
+    @api.depends("product_ids.subtotal")
+    def _calc_total(self):
+        """ Calculate the total """ 
+        for record in self:
+            self.total = sum(line.subtotal for line in record.product_ids)
+ 
+
+class Product(models.Model):
+    _name = 'product.lines'
+
+    sale_id = fields.Many2one('product.sale')
+    product_id = fields.Many2one('product.product')
+    product_name = fields.Char()
+    price = fields.Float()
+    quantity = fields.Integer(required=True, default=1)
+    subtotal = fields.Float(compute="_calc_subtotal")
+
+    @api.depends("price", "quantity")
+    def _calc_subtotal(self):
+        """ Calculate the subtotal """ 
+        for record in self:
+            record.subtotal = record.price * record.quantity
+
+    @api.onchange("product_id")
+    def _onchange_product_id(self):
+        """ To update the fields """
+        self.product_name = self.product_id.name
+        self.price = self.product_id.list_price
+
