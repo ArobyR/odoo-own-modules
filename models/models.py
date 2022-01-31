687ef9d3a452dbf107731a5cdea99595f99bf43b
ArobyR <ar.rosario001@gmail.com>
2022-01-31 10:42:54 -0400

    ProductSale model inherit
    ProductSale inherit from mail.thread and finished two methods (send_mail, create_sale_and_send_email)

--- models/models.py
+++ models/models.py
@@ -5,12 +5,13 @@ from odoo import models, fields, api
 
 class ProductSale(models.Model):
     _name = 'product.sale'
+    _inherit = ['mail.thread']
     _description = 'Product Sale'
 
-    generated_sale_id = ''
+    sale_objt = ''
 
-    partner_ids = fields.Many2many('res.partner', string="Partner")
-    product_ids = fields.One2many('product.lines', 'sale_id')
+    partner_ids = fields.Many2many("res.partner", string="Partner")
+    product_ids = fields.One2many("product.lines", "sale_id")
     total = fields.Float(compute="_calc_total", string="Total")
 
     def _get_products_formatted(self):
@@ -27,20 +28,33 @@ class ProductSale(models.Model):
             ))
         return product_lines
 
-    # Create a sale...
-    def create_sale(self):
+    # Send email
+    def send_email(self, channel=[], partner_ids=[], body=""):
+        # to create channel message_subscribe()
+        # to create mail message_post()
+        if self.message_subscribe(partner_ids, channel):
+            self.message_post(body, subject="Mensaje test")
+             
+    # Create a sale and send email...
+    def create_sale_and_send_email(self):
+        sale_names = []
         for line in self.partner_ids:
-            self.env['sale.order'].create({
+            sale_objt = self.env["sale.order"].create({
                 "partner_id": line.id,
                 "order_line": self._get_products_formatted(),
                 "date_order": fields.Date.today(),
                 }
             )
+            sale_names.append(sale_objt["name"])
+        
+        formatted_sales_names = " ".join(sale_names)
+        str_body = f"Listado de nombres: {formatted_sales_names}"
 
-    # Send an email
-    def send_email(self):
-        pass
-
+        members = self.env['mail.channel'].browse(6).channel_last_seen_partner_ids
+        partner_ids_from_channel = [p.partner_id.id for p in members]
+        
+        self.send_email([6], partner_ids_from_channel, str_body)
+  
     @api.depends("product_ids.subtotal")
     def _calc_total(self):
         """ Calculate the total """ 
@@ -51,8 +65,8 @@ class ProductSale(models.Model):
 class Product(models.Model):
     _name = 'product.lines'
 
-    sale_id = fields.Many2one('product.sale')
-    product_id = fields.Many2one('product.product')
+    sale_id = fields.Many2one("product.sale")
+    product_id = fields.Many2one("product.product")
     product_name = fields.Char()
     price = fields.Float()
     quantity = fields.Integer(required=True, default=1)
