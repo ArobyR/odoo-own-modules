44ed9e694368aa280b83e384ffa6bf24de0a0265
ArobyR <ar.rosario001@gmail.com>
2022-02-17 17:15:51 -0400

    Change the btn behaviour
    Change the btn behaviour and change the xml template

--- models/models.py
+++ models/models.py
@@ -17,77 +17,50 @@ class ReportModule(models.Model):
     views_qweb_id = fields.Many2one(
         'ir.ui.view', domain="[('type', '=', 'qweb')]", string='Vista')
 
-    def create_report(self):
-        received_list = []
-        created_by_list = []
-        client_list = []
-        for obj in self.info_report_id:
-            received_list.append(obj.received_by)
-            created_by_list.append(obj.created_by)
-            client_list.append(obj.client)
+    genered_qweb_view = fields.Many2one('ir.ui.view')
 
-        self.env['ir.ui.view'].create({"name": f'{self.views_qweb_id.name}01',
-                                       "type": self.views_qweb_id.type,
-                                       "inherit_id": self.views_qweb_id.id,
-                                       "model": self.views_qweb_id.model,
-                                       "mode": 'extension',
-                                       "priority": self.views_qweb_id.priority,
-                                       "key": self.views_qweb_id.key,
-                                       "xml_id": self.views_qweb_id.xml_id,
-                                       "arch_base": f'''<?xml version="1.0"?>
+    def create_report(self):
+        if self.genered_qweb_view:
+            self.genered_qweb_view.unlink()
+
+        description_list = []
+        for obj in self.info_report_id:
+            description_list.append(obj.description)
+
+        link = self.env['ir.ui.view'].create({"name": f'{self.views_qweb_id.name}01',
+                                              "type": self.views_qweb_id.type,
+                                              "inherit_id": self.views_qweb_id.id,
+                                              "model": self.views_qweb_id.model,
+                                              "mode": 'extension',
+                                              "priority": self.views_qweb_id.priority,
+                                              "key": self.views_qweb_id.key,
+                                              "xml_id": self.views_qweb_id.xml_id,
+                                              "arch_base": f'''<?xml version="1.0"?>
                                        <data inherit_id="{self.views_qweb_id.xml_id}" priority="{self.views_qweb_id.priority}">
                                        <xpath expr="{self.xpath_route}" position="{self.position}">  
                                         <br/>
                                         <div>
-                                          <table style="border:none display:
-                                          flex !important;">
+                                          <table style="border:none !important;">
                                             <tr style="border:none !important;">
-                                            <span
-                                            t-foreach="{self.received_list}"
-                                            t-as="i">
-    <td style="border:none !important;padding-right:10px;">___________________________________________</td>
-                                            </span>
-                                            <span
-                                            t-foreach="{self.created_by_list}"
-                                            t-as="i">
-<td style="border:none
-!important;padding-right:10px;">___________________________________________</td>
-</span>
-                                            <span
-                                            t-foreach="{self.client_list}"
-                                            t-as="i">
-<td style="border:none
-!important;padding-right:10px;">___________________________________________</td>
-</span>
+                                                <span
+                                                t-foreach="{description_list}"
+                                                t-as="i">
+        <td style="border:none !important;padding-right:10px;">___________________________________________</td>
+                                                </span>
                                             </tr>
                                             <tr class='text-center' style="border:none !important;">
                                               <span
-                                              t-foreach="{self.received_list}"
-                                              t-as="i">
-                                                  <td style="border:none
-                                                  !important;">Recibido por <t
-                                                  t-esc="i"/></td>
-                                              </span>
-                                              <span
-                                              t-foreach="{self.created_by_list}"
-                                              t-as="i">
-                                                  <td style="border:none
-                                                  !important;">Autorizado por
-                                                  <t t-esc="i"/></td>
-                                              </span>
-                                              <span
-                                              t-foreach="{self.client_list}"
-                                              t-as="i">
-                                              <td style="border:none
-                                              !important;">Cliente <t t-esc="i"/></td>
+                                                  t-foreach="{description_list}"
+                                                  t-as="i"><td style="border:none 
+                                                  !important;"><t t-esc="i"/></td>
                                               </span>
                                             </tr>
-                        
                                           </table>
                                         </div>
                                        </xpath>
                                        </data>'''
-                                       })
+                                              })
+        self.genered_qweb_view = link.id
 
 
 class InfoReport(models.Model):
@@ -95,6 +68,4 @@ class InfoReport(models.Model):
     _description = 'Informacion del reporte'
 
     report_module_id = fields.Many2one('report.module')
-    received_by = fields.Char(string='Recibido por:')
-    created_by = fields.Char(string='Creado por:')
-    client = fields.Char(string='Cliente:')
+    description = fields.Char(string='Descripcion:')
