0ef6640675f977a3db88805a338ef86117f3f983
ArobyR <ar.rosario001@gmail.com>
2022-08-01 09:54:03 -0400

    updated author and website in the manifest

--- sale_report/__manifest__.py
+++ sale_report/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "ArobyR",
-    'website': "",
+    'website': "https://github.com/ArobyR",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
