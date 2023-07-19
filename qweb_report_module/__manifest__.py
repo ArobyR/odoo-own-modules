452aad3b06cd99303b850341fcadbb682a0a1750
ArobyR <ar.rosario001@gmail.com>
2023-07-19 09:29:41 -0400

    add license

--- qweb_report_module/__manifest__.py
+++ qweb_report_module/__manifest__.py
@@ -17,6 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
+    "license": "LGPL-3",
     'version': '0.1',
 
     # any module necessary for this one to work correctly
